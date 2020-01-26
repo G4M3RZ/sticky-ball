9be7001b1d4a363c1275a47a12994bb29893fd99
Pablo <pablo.0698@hotmail.com>
2020-01-25 21:56:24 -0500

    sticky ball

---
+++ Assets/Scripts/Player/Impulso.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impulso : MonoBehaviour {
+
+    public Resortera _touchGlobal;
+    public GameObject _flecha;
+
+    public Rigidbody2D _rb;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (!_touchGlobal.canDrag)
+        {
+            _rb.constraints = ~RigidbodyConstraints2D.FreezePosition;
+            _rb.gravityScale = 1.3f;
+            _rb.AddForce(-_flecha.transform.up * 500);
+            _touchGlobal.canDrag = true;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "wall")
+        {
+            _rb.constraints = ~RigidbodyConstraints2D.FreezePositionX;
+            _rb.gravityScale = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Resortera.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Resortera : MonoBehaviour {
+
+    public Transform _pivot;
+    public float _springRange;
+    public float _maxVel;
+
+    public GameObject _flecha;
+    public Transform _player;
+
+    public Rigidbody2D _rb;
+
+    Vector3 initPos = new Vector3(0, 2, 1.1f);
+    Vector3 endPos = new Vector3(0, 0, 1.1f);
+
+    public bool canDrag = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _rb.GetComponent<Rigidbody2D>();
+        _rb.bodyType = RigidbodyType2D.Kinematic;
+	}
+    private void Update()
+    {
+        if (!canDrag)
+        {
+            _flecha.transform.localPosition = Vector3.Lerp(transform.localPosition, endPos, Time.deltaTime);
+        }
+        FlechaFace();
+    }
+    void OnMouseDrag()
+    {
+        if (!canDrag)
+            return;
+        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var dis = pos - _pivot.position;
+        dis.z = 0;
+        if (dis.magnitude > _springRange)
+        {
+            dis = dis.normalized * _springRange;
+        }
+        _flecha.transform.localPosition = initPos + dis + _pivot.position;
+    }
+    void OnMouseUp()
+    {
+        if (!canDrag)
+            return;
+        canDrag = false;
+    }
+    void FlechaFace()
+    {
+        if (!canDrag)
+            return;
+        Vector2 direccion = new Vector2(
+            _player.transform.position.x - _flecha.transform.position.x,
+            _player.transform.position.y - _flecha.transform.position.y);
+        _flecha.transform.up = direccion;
+    }
+}
